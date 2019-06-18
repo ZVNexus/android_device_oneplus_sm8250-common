b4bb4ffeca9a4eba5b57c502d9bb09717a313c32
Peter Cai <peter@typeblog.net>
2019-06-19 07:57:06 +0800

    sm8250-common: Import keylayout from OOS 10.5.9.
    Change-Id: I6a8262f04b749f020cb20293444184190155e48a

--- common.mk
+++ common.mk
@@ -57,6 +57,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/keylayout/fpc1020.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/fpc1020.kl \
+	$(LOCAL_PATH)/keylayout/gf_input.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gf_input.kl \
+	$(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
+
 # NFC
 PRODUCT_PACKAGES += \
     NfcNci \
